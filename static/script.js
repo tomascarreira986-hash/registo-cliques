98d8ac33ae7b370cc7022798d1911f03dae134f9
tomascarreira986-hash <tomascarreira986@gmail.com>
2026-01-22 16:39:53 +0000

    script.js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ function registarClique(botao) {
         .then(response => response.json())
         .then(data => {
             document.getElementById("resultado").innerHTML = `
-                <p><strong>Número:</strong> ${data.numero}</p>
+                <p><strong>Número do registo:</strong> ${data.numero}</p>
                 <p><strong>Data:</strong> ${data.data}</p>
                 <p><strong>Hora:</strong> ${data.hora}</p>
             `;
