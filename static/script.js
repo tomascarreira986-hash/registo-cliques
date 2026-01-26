69eb49b8896224383632ed5fb94cbdf598229082
tomascarreira986-hash <tomascarreira986@gmail.com>
2026-01-26 08:43:38 +0000

    script.js

--- static/script.js
+++ static/script.js
@@ -2,10 +2,20 @@ function registarClique(botao) {
     fetch(`/click/${botao}`)
         .then(response => response.json())
         .then(data => {
+
             document.getElementById("resultado").innerHTML = `
-                <p><strong>Número do registo:</strong> ${data.numero}</p>
+                <p><strong>Número do clique:</strong> ${data.numero}</p>
                 <p><strong>Data:</strong> ${data.data}</p>
                 <p><strong>Hora:</strong> ${data.hora}</p>
             `;
+
+            const lista = document.getElementById("listaDias");
+            lista.innerHTML = "";
+
+            data.dias.forEach(dia => {
+                const li = document.createElement("li");
+                li.textContent = `${dia.data} – ${dia.total} cliques`;
+                lista.appendChild(li);
+            });
         });
 }
